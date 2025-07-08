ecb9cadb5661f08c07130d77bb2b89d0428803e3
Kartik Garg <kartikamitgarg2005@gmail.com>
2025-07-08 11:21:18 +0530

    util code disscussion

---
+++ src/utils/APIErrors.js
@@ -0,0 +1,19 @@
+class ApiErros extends Error{
+    constructor(
+        statuscode,
+        message="Something went wrong",
+        errors = [],
+        stack = ""
+    )
+    {
+        super(message),
+        this.statuscode = statuscode,
+        this.data = null,
+        this.message = message,
+        this.success = false,
+        this.errors = errors
+    }
+}
+
+
+export {ApiErros}
